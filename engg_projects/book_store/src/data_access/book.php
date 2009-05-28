f36ba254ebf542f34cd898a83c244d9edd5074ed
Mohammed Ashfaq <imstar_star@yahoo.com>
2009-05-28 17:06:45 +0530

    addition of book

--- engg_projects/book_store/src/data_access/book.php
+++ engg_projects/book_store/src/data_access/book.php
@@ -1,14 +1,14 @@
 <?php
-  class bookEntry{
-    public $id;
+  class book{
+    public $book_id;
     public $book_name;
     public $book_author;
     public $book_publisher;
     public $book_price;
     
     
-    function bookEntry( $id, $book_name,$book_author, $book_publisher, $book_price){
-      $this->id = $id;
+    function book( $book_id, $book_name,$book_author, $book_publisher, $book_price){
+      $this->book_id = $book_id;
       $this->book_name = $book_name;
       $this->book_author = $book_author;
       $this->book_publisher = $book_publisher;
--- engg_projects/book_store/src/dump/add_book1.php
+++ engg_projects/book_store/src/dump/add_book1.php
@@ -6,7 +6,7 @@
     <link type="text/css" rel="stylesheet" href="style.css " />
   </head>
   <body>
-<!--
+
 <div id="header">
        <div id="search" align="right">
            <form action="index.php" method="post">
@@ -29,73 +29,67 @@
 <b>ADD NEW BOOK TO DATABASE</b>
 
 <div id ="content">
-<form action="send_mail.php" method="post">
+<form action="add_book.php" method="post">
+<label for="id">ID</lable>  &nbsp <input type="text" id="id" name="id" size="100"/><br/>
 <label for="name">NAME:</lable> &nbsp &nbsp &nbsp <input type="text" id="name" name="name" size="100"/><br/>
 </br>
 <label for="author">Author :</lable> &nbsp <input type="text" id="author" name="author" size="101" /><br/>
 <label for="publisher">Publisher :</lable> &nbsp <input type="text" id="publisher" name="publisher" size="101" /><br/>
 <label for="price">Price:</lable> &nbsp <input type="text" id="price" name="price" size="101" /><br/>
 </br>
-Description :&nbsp <br> <textarea rows=12 cols=84 id="description" name="description"></textarea><br/>
+
 <input type="submit" value="ADD"> &nbsp <input type="reset" value="Reset">
 </form>
 
 </div>
 
-<div id ="footer">
-</div>
-
--->
-
+<div id ="footer"></div>
 
 <?php
-/*
+ 
 
 require_once 'data_access/book_class.php';
   
-  if(isset($_POST['fromid']))
+  if(isset($_POST[id]))
   
   {
   
-  $mail_repo = new bookRepo();
+  $book_repo = new bookRepo();
 $bookids = $book_repo->get_bookids();
 
-
-if(!$bookids)
+/*if(!$bookids)
 {
 echo " <h3> There are no existing book entries </h3> ";
 }
-else
+else*/
 {
 
- 
- 
-      $headers = "From:".$_POST['fromid'];
-      $subject = $_POST['subject'];
-      $message = $_POST['mbody'];
-      
-   
-   $to = $_POST['fromid'] ;
-   mail($to, $subject, $message, $headers) ;  
+
+      $id  = $_POST['id'];
+      $name  = $_POST['name'];
+      $author  = $_POST['author'];
+      $publisher  = $_POST['publisher'];
+      $price  = $_POST['price'];
       
       
- foreach( $mailids as $mailid )
-        {
- 
-  $to = $mailid->email_id ;
- // echo "$to <br/>";
- //echo "result".
-  mail($to, $subject, $message, $headers) ;
- 
+      
+      $bookid = new bookEntry($id,$name,$author,$publisher,$price);
+      $book_repo = new bookRepo();
+      
+      $result = $book_repo->insert_bookid( $bookid );
+      
+      if( $result ){
          }
         
-      echo " <h2 style= 'color :green'> 	Your message has been sent</h2> ";
+      echo " <h2 style= 'color :green'> 	Book has been added</h2> ";
 
   }    
-     
+    
     }
- */       
+        
 ?> 
+
+
 
 </body>
 </html>

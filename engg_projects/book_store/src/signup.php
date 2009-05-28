c61ba487f465efe84994aa06ad368a70f9188c2a
Mohammed Ashfaq <imstar_star@yahoo.com>
2009-05-28 19:56:06 +0530

    add signup page

--- engg_projects/book_store/src/signup.php
+++ engg_projects/book_store/src/signup.php
@@ -1,23 +1,32 @@
 <html>
 <head>
-<tile>
-this is a sign up page</tile>
+<title>
+this is a sign up page</title>
 </head>
 <body>
-<h1>welcome to signup</h1>
+
 <div id="header">
 <div id="signup" align="middle">
+<h1>welcome to signup</h1>
 <form action="index.php" method="post">
 <br> 
-<b></b>username</b>
-<input type="text" name="username" value="" id="signup">
-<b>password</b>
-<input type="password" name="password" value="" id="signup">
-<b>Confirm password</b>
-<input type="password" name="password" value="" id="signup">
+<b>userid:</b>
+<input type="text" name="userid" value="" id="signup"></br>
+<b>password:</b>
+<input type="password" name="password" value="" id="signup"></br>
+<b>Confirm password:</b>
+<input type="password" name="password" value="" id="signup"></br>
+<b>gender:</b>&nbsp&nbsp&nbsp&nbsp&nbsp
+<b>male</b><input type="radio" name="male" value="" id="signup">&nbsp&nbsp
+<b>female</b><input type="radio" name="female" value="" id="signup"><br>
+<b>firstname:</b>
+<input type="text" name="first ame" value="" id="signup"></br>
+<b>lastname:</b>
+<input type="text" name="lastname" value="" id="signup"></br>
 
 
-<input type=submit name=submit value="GO" id=submit>
+<input type="button" name="submit" value="signup" id="submit">
+<input type="reset" name="reset" value="reset" id="submit">
 </form>
 </div></div>
 </body></html>

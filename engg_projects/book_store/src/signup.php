8fd1bd6e62b4623c3ea21a07e5d4e1d25d332fdb
Mohammed Ashfaq <imstar_star@yahoo.com>
2009-05-29 21:07:02 +0530

    added header on signup page

--- engg_projects/book_store/src/signup.php
+++ engg_projects/book_store/src/signup.php
@@ -1,9 +1,52 @@
 <html>
 <head>
 <title>
-this is a sign up page</title>
+this is a sign up page</title> <style type="text/css">
+<!--
+a:link {color: #400000; text-decoration: underline; }
+a:active {color: #0000ff; text-decoration: underline; }
+a:visited {color: #000000; text-decoration: underline; }
+a:hover {color: #ff0000; text-decoration: none; }
+-->
+
+.search_result{
+background-color:#00BFFF;
+}
+#header_table{
+width:100%;
+background-color:#000066;
+color:#ffffff;
+}
+
+#header{
+background-color:#000066;
+color:#ffffff;
+}
+   </style>
+   
 </head>
 <body>
+	<div id="header">
+
+<div id="search" align="right">
+<form action="admin.php" method="GET">
+<input type=submit name=submit value="LOGOUT" id=submit>
+</form>
+</div>
+ 
+<div id = "heading" align="center">
+<h1>Welcome to Admin's Home Page</h1>
+</div>
+
+</div>
+
+<div id="searchbar" align="center">
+<form action="admin.php" method="GET">
+ SEARCH:<input type="text" name="search" id="search" value="" size="50">
+<input type="submit"  value="GO">&nbsp <a href="advance_search.php"><b><i>ADVANCE SEARCH</i></b></a>
+</form>
+</div>
+
 
 <div id="header">
 <div id="signup" align="middle">
